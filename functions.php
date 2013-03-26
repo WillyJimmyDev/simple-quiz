d23a5739b9126e9f00a01b6f4dd1ab8b3f479383
ElanMan <ben@elanman.com>
2013-03-26 20:56:56 +0000

    Using spl_autolaod_register instead of __autololad - enablng multiple auto load paths

--- functions.php
+++ functions.php
@@ -1,9 +1,19 @@
 <?php
 define('CLASS_DIR','classes/');
 
-function __autoload($classname)
+function classloader($classname)
 {
     $path = CLASS_DIR . strtolower($classname) . '.class.php';
     require($path);
 }
+
+spl_autoload_register('classloader');
+
+function baseclassloader($classname)
+{
+    $path = CLASS_DIR . 'base/' .strtolower($classname) . '.class.php';
+    require($path);
+}
+
+spl_autoload_register('baseclassloader');
 ?>
