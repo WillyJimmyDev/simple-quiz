9d76a1118a2158bceb18f02ad1a52f8147a9f309
ElanMan <ben@elanman.com>
2015-05-03 09:40:29 +0100

    Added comments and example settings in SampleConfig.php.

--- SimpleQuiz/Utils/Base/SampleConfig.php
+++ SimpleQuiz/Utils/Base/SampleConfig.php
@@ -4,14 +4,14 @@ namespace SimpleQuiz\Utils\Base;
 class Config
 {
 
-    //used whe sending emails to users
+    //used when sending emails to users
     static $appname = 'SimpleQuiz';
-    static $siteurl = '';
-    static $appEmail = '';
+    static $siteurl = ''; // e.g https://simplequiz.co.uk/
+    static $appEmail = '';// email address used in the 'From' header sent in all emails e.g admin@yoursite.com
 
-    static $templateDir = '../templates';
+    static $emailTemplateDir = '/templates/email/'; // not currently used
 
-    //database credentials
+    //database credentials . Change as required
     static $dbhost = 'localhost';
     static $dbname = 'simple-quiz';
     static $dbuser = '';
@@ -20,7 +20,15 @@ class Config
     //only used if requireauth == false
     static $defaultUser = 'default';
 
-    //mailer config
+    //mailer config. Change as required.
+    /*
+    gmail example
+    static $mailHost = 'smtp.gmail.com';
+    static $mailUser = 'example@gmail.com';
+    static $mailPass = '1234';
+    static $mailPort = '587';
+    */
+
     static $mailHost = '';
     static $mailUser = '';
     static $mailPass = '';
